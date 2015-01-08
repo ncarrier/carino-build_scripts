222210a91a62a3e9f36b6ac9bbe52e68b1b7bad3
Nicolas CARRIER <carrier.nicolas0@gmail.com>
2015-01-08 21:31:06 +0100

    renamed kernel build script to linux-carbuild.sh

--- linux.carbuild.sh
+++ linux.carbuild.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
-package_name=kernel
+package_name=linux
 
 build_dir=${BUILD_DIR}/${package_name}
 mkdir -p ${build_dir}
 
 cp ${CONFIG_DIR}/kernel_config ${build_dir}/.config
 
-cd ${PACKAGES_DIR}/linux
+cd ${PACKAGES_DIR}/${package_name}
 ARCH=arm CROSS_COMPILE=${TOOLCHAIN_PREFIX} \
      LOADADDR=0x40008000 \
      make uImage dtbs O=${build_dir} -j 8

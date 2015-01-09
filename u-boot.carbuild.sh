83c106b614381599461ec31bbf491c329dd1c71c
Nicolas CARRIER <carrier.nicolas0@gmail.com>
2015-01-09 07:08:18 +0100

    install the u-boot produced files in the right place

--- u-boot.carbuild.sh
+++ u-boot.carbuild.sh
@@ -5,13 +5,19 @@ package_name=u-boot
 build_dir=${BUILD_DIR}/${package_name}
 mkdir -p ${build_dir}
 
+# use the default u-boot pcduino3 configuration
 cd ${PACKAGES_DIR}/${package_name}
+# TODO update the config only if needed
 make CROSS_COMPILE=${TOOLCHAIN_PREFIX} \
 		   Linksprite_pcDuino3_config \
 		   O=${build_dir}
 
+# do build
 make CROSS_COMPILE=${TOOLCHAIN_PREFIX} \
 		   O=${build_dir} \
 		   -j 8
 
+# install the needed bits at the right place
+cp ${build_dir}/u-boot-sunxi-with-spl.bin ${U_BOOT_DIR}/
+cp ${CONFIG_DIR}/uEnv.txt ${BOOT_DIR}/
 

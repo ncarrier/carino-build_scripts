7df42461cafd2c533607086eac72b19c7fe8e5c0
Nicolas CARRIER <carrier.nicolas0@gmail.com>
2015-01-09 07:08:01 +0100

    install the kernel produced files in the right place

--- linux.carbuild.sh
+++ linux.carbuild.sh
@@ -5,10 +5,16 @@ package_name=linux
 build_dir=${BUILD_DIR}/${package_name}
 mkdir -p ${build_dir}
 
+# use the versionned kernel configuration
 cp ${CONFIG_DIR}/kernel_config ${build_dir}/.config
 
+# do build
 cd ${PACKAGES_DIR}/${package_name}
 ARCH=arm CROSS_COMPILE=${TOOLCHAIN_PREFIX} \
      LOADADDR=0x40008000 \
      make uImage dtbs O=${build_dir} -j 8
+
+# install the needed bits at the right place
+cp ${build_dir}/arch/arm/boot/dts/sun7i-a20-pcduino3.dtb ${BOOT_DIR}/
+cp ${build_dir}/arch/arm/boot/uImage ${BOOT_DIR}/
 
